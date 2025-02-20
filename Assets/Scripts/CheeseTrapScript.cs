d54c9cc363248198f36c088b500ca6a20a1cfda9
Ravindu Hewabaddage <hewabadd@shernet.sheridancollege.ca>
2025-02-20 15:23:22 -0500

    created working cheese trap added in player created playerscript

--- Assets/Scripts/CheeseTrapScript.cs
+++ Assets/Scripts/CheeseTrapScript.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class CheeseTrapScript : MonoBehaviour
 {
+    public float timerValue;
+    public float timerGap;
+    public float positionScale;
+    private Vector2 startingPosition;
+    public AnimationCurve curve;
+    private float t=0f;
+    private Vector2 position;
     // Start is called before the first frame update
     void Start()
     {
-        
+        position = transform.position;
+        startingPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        t += Time.deltaTime/timerValue;
+        position.y = curve.Evaluate(t%timerGap);
+        position.y *= positionScale;
+        position.y += startingPosition.y;
+        transform.position = position;
+
         
     }
 }
